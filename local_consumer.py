5ecef87efdaced04673c71ca17dcc798c988ccbb
Shihab Thanaparamban <98036345+shihabkbasheer03@users.noreply.github.com>
2026-01-18 14:58:25 +0400

    Refactor Kinesis client initialization and record handlingregion

--- local_consumer.py
+++ local_consumer.py
@@ -2,23 +2,24 @@ import boto3
 import time
 import json
 
+REGION = "eu-north-1"
+STREAM_NAME = "IoT_Sensor_Stream"
+SHARD_ID = "shardId-000000000002"   # or 000000000000 / 000000000001
 
-client = boto3.client('kinesis')
+client = boto3.client("kinesis", region_name=REGION)
+
 shardIterator = client.get_shard_iterator(
-    StreamName='IoT_Sensor_Stream',
-    ShardId='shardId-000000000002',
-    ShardIteratorType='LATEST',
-)['ShardIterator']
-
+    StreamName=STREAM_NAME,
+    ShardId=SHARD_ID,
+    ShardIteratorType="LATEST",   # or "TRIM_HORIZON"
+)["ShardIterator"]
 
 while True:
-    response = client.get_records(
-        ShardIterator=shardIterator
-    )
-    shardIterator = response['NextShardIterator']
-    if len(response['Records']) > 0:
-        for item in response['Records']:
-            readings = json.loads(item["Data"])
+    response = client.get_records(ShardIterator=shardIterator, Limit=100)
+    shardIterator = response["NextShardIterator"]
+
+    for item in response.get("Records", []):
+        readings = json.loads(item["Data"].decode("utf-8"))  # Data is bytes
         print(readings)
 
     time.sleep(0.2)
